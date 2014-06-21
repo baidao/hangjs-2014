814c5186c79f53b82240e341befdd87cfdae452c
James Halliday <mail@substack.net>
2014-06-21 11:20:43 +0800

    punchline

---
+++ example/app/999.js
@@ -0,0 +1,22 @@
+/*
+
+  let's create a messaging web app using:
+ 
+  * ecstatic for static files
+  * routes for routing
+  * leveldb as the database
+  * trumpet for server-side html rendering
+  * hyperspace for shared server<->browser rendering
+  * multilevel to use leveldb from the browser
+  * shoe for websockets as a stream
+  * browserify to use npm packages in the browser
+
+  $ wc -l *.js render/*.js
+  
+  15 finished/browser.js
+  21 finished/router.js
+  21 finished/server.js
+  13 finished/render/msg.js
+  70 total
+
+*/
